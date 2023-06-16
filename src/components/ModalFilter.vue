5dc3ac049b008d1d6cadb291c6414d195d27a2dc
giuliatognali <118864811+giuliatognali@users.noreply.github.com>
2023-06-16 19:01:05 +0200

    scaffolding modal reaserch

---
+++ src/components/ModalFilter.vue
@@ -0,0 +1,80 @@
+<script>
+
+export default {
+    name: "ModalFilter",
+}
+</script>
+
+<template>
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Ricerca avanzata</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <h5>Numero Stanze</h5>
+                        <div>
+                            <ul class="px-0 pt-2">
+                                <li class="filter-select">
+                                    <button class="btn active">Qualsiasi</button>
+                                    <button class="btn">1</button>
+                                    <button class="btn">2</button>
+                                    <button class="btn">3</button>
+                                    <button class="btn">4</button>
+                                    <button class="btn">5</button>
+                                    <button class="btn">6+</button>
+                                </li>
+                            </ul>
+                        </div>
+                        <h5>Numero Bagni</h5>
+                        <div>
+                            <ul class="px-0 pt-2">
+                                <li class="filter-select">
+                                    <button class="btn ">Qualsiasi</button>
+                                    <button class="btn">1</button>
+                                    <button class="btn active">2</button>
+                                    <button class="btn">3</button>
+                                    <button class="btn">4</button>
+                                    <button class="btn">5</button>
+                                    <button class="btn">6+</button>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-negative" data-bs-dismiss="modal">Annulla</button>
+                        <button type="button" class="btn btn-positive">Applica</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+</template>
+
+<style lang="scss" scoped>
+.modal-content{
+    @import '../assets/partials/variables';
+    border: solid 3px $custom-blu;
+    border-radius: 1.5625rem;
+    .filter-select{
+        button{
+            border: solid 1px $custom-black;
+            border-radius: 1.5625rem ;
+            margin-right: 10px;
+            padding: 3px 15px;
+        }
+        .active{
+                background-color: $custom-blu;
+                color: $custom-black;
+                font-weight: 600;
+            }
+    }
+    .btn-positive{
+        background-color: $custom-blu;
+    }
+    .btn-negative{
+        background-color: $custom-red;
+    }
+}
+</style>
