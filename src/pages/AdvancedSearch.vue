8fda0d0e47652207ca58073b91ec5696da544e57
giuliatognali <118864811+giuliatognali@users.noreply.github.com>
2023-06-16 16:54:17 +0200

    carousel card-img

--- src/pages/AdvancedSearch.vue
+++ src/pages/AdvancedSearch.vue
@@ -1,14 +1,47 @@
-<template>
-    <div>
-        <h1 class="text-center">AdvancedSearch Page</h1>
-    </div>
-</template>
-
 <script>
-
+import CardImg from '../components/icons/CardImg.vue';
 export default {
     name: "AdvancedSearch",
+    components: {
+        CardImg
+    }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<template>
+    <div class="container ">
+
+        <div class="custom-container py-5">
+            <div class="input-group rounded pb-5">
+                <input type="search" class="form-control rounded" aria-label="Search" placeholder="Old-ricerca"
+                    aria-describedby="search-addon" />
+                <a href="" class="icon ps-4">
+                    <font-awesome-icon icon="magnifying-glass" />
+                </a>
+                <a href="" class="icon ps-4">
+                    <font-awesome-icon icon="sliders" />
+                </a>
+            </div>
+        </div>
+            <div class="container p-0 d-flex justify-content-between">
+                <CardImg />
+            <CardImg />
+            <CardImg />
+            <CardImg />
+            </div>
+
+
+            
+
+
+
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@import '../assets/partials/variables';
+
+.icon {
+    font-size: 1.75rem;
+    color: $custom-black;
+}</style>
