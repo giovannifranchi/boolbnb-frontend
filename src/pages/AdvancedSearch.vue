b5c4c36d0aa2bd95b84120748a6b48ea97d301c2
giuliatognali <118864811+giuliatognali@users.noreply.github.com>
2023-06-16 17:00:30 +0200

    insert modal

--- src/pages/AdvancedSearch.vue
+++ src/pages/AdvancedSearch.vue
@@ -10,7 +10,7 @@ export default {
 
 <template>
     <div class="container ">
-
+        <!-- search bar -->
         <div class="custom-container py-5">
             <div class="input-group rounded pb-5">
                 <input type="search" class="form-control rounded" aria-label="Search" placeholder="Old-ricerca"
@@ -18,23 +18,37 @@ export default {
                 <a href="" class="icon ps-4">
                     <font-awesome-icon icon="magnifying-glass" />
                 </a>
-                <a href="" class="icon ps-4">
+                <button type="button" class="btn ps-4 icon" data-bs-toggle="modal" data-bs-target="#exampleModal">
                     <font-awesome-icon icon="sliders" />
-                </a>
+                </button>
             </div>
         </div>
-            <div class="container p-0 d-flex justify-content-between">
-                <CardImg />
-            <CardImg />
-            <CardImg />
-            <CardImg />
+        <!-- /search bar -->
+
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
             </div>
-
-
-            
-
-
-
+        </div>
+        <!-- Modal -->
+        <div class="container p-0 d-flex justify-content-between">
+            <CardImg />
+            <CardImg />
+            <CardImg />
+            <CardImg />
+        </div>
     </div>
 </template>
 
@@ -44,4 +58,5 @@ export default {
 .icon {
     font-size: 1.75rem;
     color: $custom-black;
-}</style>
+}
+</style>
