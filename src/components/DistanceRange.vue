9f81c95d282b6fed41cbecb54dd66c0e9720da41
giuliatognali <118864811+giuliatognali@users.noreply.github.com>
2023-06-19 18:31:26 +0200

    feat price and distance range

---
+++ src/components/DistanceRange.vue
@@ -0,0 +1,49 @@
+<script>
+export default {
+    name: "PriceRange",
+
+}
+</script>
+<template>
+    <div class="slidecontainer mb-3 mt-2" >
+        <h5> Distanza</h5>
+        <input type="range" min="1" max="100" value="20" class="slider" id="myRange">
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+    @import '../assets/partials/variables';
+   .slider {
+        -webkit-appearance: none;
+        width: 100%;
+        height: 15px;
+        border-radius: 5px;
+        background: $custom-white;
+        outline: none;
+        opacity: 0.7;
+        -webkit-transition: .2s;
+        transition: opacity .2s;
+    }
+
+    .slider::-webkit-slider-thumb {
+        -webkit-appearance: none;
+        appearance: none;
+        width: 25px;
+        height: 25px;
+        border-radius: 50%;
+        background: $custom-blu;
+        cursor: pointer;
+    }
+
+    .slider::-moz-range-thumb {
+        width: 25px;
+        height: 25px;
+        border-radius: 50%;
+        background: $custom-blu;
+        cursor: pointer;
+    }
+
+    /* /distance range */
+
+</style>
--- src/components/ModalFilter.vue
+++ src/components/ModalFilter.vue
@@ -1,103 +1,150 @@
 <script>
 import { mapActions, mapGetters } from "vuex";
+import PriceRange from "../components/PriceRange.vue"
+import DistanceRange from "../components/DistanceRange.vue"
 export default {
     name: "ModalFilter",
-    data() {
-    return {
-      isbusy: true
-    };
-    
-},
-async created() {
-    await this.fetchServices();
-    this.isbusy = false;
-  },
+    components: {
+        PriceRange,
+        DistanceRange
+    },
 
-computed: {
-    ...mapGetters(["getServices"]),
-  },
-  methods: {
-    ...mapActions(["fetchServices"])
-}
+    data() {
+        return {
+            isbusy: true
+        };
+
+    },
+    async created() {
+        await this.fetchServices();
+        this.isbusy = false;
+    },
+
+    computed: {
+        ...mapGetters(["getServices"]),
+    },
+    methods: {
+        ...mapActions(["fetchServices"])
+    }
 
 }
 </script>
 
 <template>
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="exampleModalLabel">Ricerca avanzata</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="ModalFilter" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h3 class="modal-title" id="ModalFilter">Ricerca avanzata</h3>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <!-- main modal -->
+                <div class="modal-body">
+                    <DistanceRange />
+                    <PriceRange />
+                    <!-- Rooms -->
+                    <div>
                         <h5>Numero Stanze</h5>
-                        <div>
-                            <ul class="px-0 pt-2">
-                                <li class="filter-select">
-                                    <button class="btn active">Qualsiasi</button>
-                                    <button class="btn">1</button>
-                                    <button class="btn">2</button>
-                                    <button class="btn">3</button>
-                                    <button class="btn">4</button>
-                                    <button class="btn">5</button>
-                                    <button class="btn">6+</button>
-                                </li>
-                            </ul>
-                        </div>
+                        <ul class="px-0 pt-2">
+                            <li class="filter-select">
+                                <button class="btn active">Qualsiasi</button>
+                                <button class="btn">1</button>
+                                <button class="btn">2</button>
+                                <button class="btn">3</button>
+                                <button class="btn">4</button>
+                                <button class="btn">5</button>
+                                <button class="btn">6+</button>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- /Rooms -->
+                    <!-- beds -->
+                    <div>
+                        <h5>Numero Letti</h5>
+                        <ul class="px-0 pt-2">
+                            <li class="filter-select">
+                                <button class="btn active">Qualsiasi</button>
+                                <button class="btn">1</button>
+                                <button class="btn">2</button>
+                                <button class="btn">3</button>
+                                <button class="btn">4</button>
+                                <button class="btn">5</button>
+                                <button class="btn">6+</button>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- /beds -->
+                    <!-- bathrooms -->
+                    <div>
                         <h5>Numero Bagni</h5>
-                        <div>
-                            <ul class="px-0 pt-2">
-                                <li class="filter-select">
-                                    <button class="btn ">Qualsiasi</button>
-                                    <button class="btn">1</button>
-                                    <button class="btn active">2</button>
-                                    <button class="btn">3</button>
-                                    <button class="btn">4</button>
-                                    <button class="btn">5</button>
-                                    <button class="btn">6+</button>
-                                </li>
-                            </ul>
-                        </div>
-                        <div v-if="!isbusy">
+                        <ul class="px-0 pt-2">
+                            <li class="filter-select">
+                                <button class="btn ">Qualsiasi</button>
+                                <button class="btn">1</button>
+                                <button class="btn active">2</button>
+                                <button class="btn">3</button>
+                                <button class="btn">4</button>
+                                <button class="btn">5</button>
+                                <button class="btn">6+</button>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- /bathrooms -->
+                    <!-- services -->
+                    <div v-if="!isbusy">
                         <h5>Servizi</h5>
-                        <div class="form-check" v-for="service in getServices">                                
-                               {{service.name}}
+                        <div class="form-check" v-for="service in getServices">
+                            <input class="form-check-input" type="checkbox" value="" id="check-service">
+                            <label class="form-check-label" for="check-service">
+                                <div>
+                                    <font-awesome-icon aria-expanded="false" :icon="service.icon_url" class="icon" />
+                                    {{ service.name }}
+
+                                </div>
+                            </label>
                         </div>
-                     </div>
                     </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-negative" data-bs-dismiss="modal">Annulla</button>
-                        <button type="button" class="btn btn-positive">Applica</button>
-                    </div>
+                    <!-- /services -->
+                </div>
+                <!-- /main modal -->
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-negative" data-bs-dismiss="modal">Annulla</button>
+                    <button type="button" class="btn btn-positive">Applica</button>
                 </div>
             </div>
         </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
-.modal-content{
+.modal-content {
     @import '../assets/partials/variables';
     border: solid 3px $custom-blu;
     border-radius: 1.5625rem;
-    .filter-select{
-        button{
+.modal-title{
+    color: $custom-red;
+}
+    .filter-select {
+        button {
             border: solid 1px $custom-black;
-            border-radius: 1.5625rem ;
+            border-radius: 1.5625rem;
             margin-right: 10px;
             padding: 3px 15px;
         }
-        .active{
-                background-color: $custom-blu;
-                color: $custom-black;
-                font-weight: 600;
-            }
+
+        .active {
+            background-color: $custom-blu;
+            color: $custom-black;
+            font-weight: 600;
+        }
     }
-    .btn-positive{
+
+
+    .btn-positive {
         background-color: $custom-blu;
     }
-    .btn-negative{
+
+    .btn-negative {
         background-color: $custom-red;
     }
 }
