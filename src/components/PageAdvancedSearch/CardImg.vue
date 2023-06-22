6bd12f537e136b10652d442928f6935041e488d0
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-06-22 15:54:44 +0200

    Add: router link to card image component

--- src/components/PageAdvancedSearch/CardImg.vue
+++ src/components/PageAdvancedSearch/CardImg.vue
@@ -1,91 +1,110 @@
 <template>
-    <div class="card col-lg-4 col-md-4 col-sm-12 mb-5">
-        <!-- carosello -->
-        <div :id="'carouselIndicators_' + uniqueId" class="carousel slide">
-            <ul class="carousel-indicators">
-                <li v-for="(image, index) in getAllImages" :key="index" :data-bs-target="'#carouselIndicators_' + uniqueId"
-                    :data-bs-slide-to="index" :class="{ active: index === activeIndex }"></li>
-            </ul>
-            <div class="carousel-inner">
-                <div :class="['carousel-item', { active: index === activeIndex }]" v-for="(image, index) in getAllImages"
-                    :key="index">
-                    <img :src="image" class="d-block w-100" alt="...">
-                </div>
-            </div>
-            <button class="carousel-control-prev" type="button" :data-bs-target="'#carouselIndicators_' + uniqueId"
-                data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" :data-bs-target="'#carouselIndicators_' + uniqueId"
-                data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
+  <div class="card col-lg-4 col-md-4 col-sm-12 mb-5">
+    <!-- carosello -->
+    <router-link
+      :to="{ name: 'apartment', params: { slug: dataApartment.slug, id: dataApartment.id } }"
+      class="text-decoration-none text-reset"
+    >
+      <div :id="'carouselIndicators_' + uniqueId" class="carousel slide">
+        <ul class="carousel-indicators">
+          <li
+            v-for="(image, index) in getAllImages"
+            :key="index"
+            :data-bs-target="'#carouselIndicators_' + uniqueId"
+            :data-bs-slide-to="index"
+            :class="{ active: index === activeIndex }"
+          ></li>
+        </ul>
+        <div class="carousel-inner">
+          <div
+            :class="['carousel-item', { active: index === activeIndex }]"
+            v-for="(image, index) in getAllImages"
+            :key="index"
+          >
+            <img :src="image" class="d-block w-100" alt="..." />
+          </div>
         </div>
-        <!-- /carosello -->
-        <div class="info-card">
-            <h5 class="card-title pt-2">{{ dataApartment.address }}, {{ dataApartment.city }}</h5>
-            <p>{{ dataApartment.name }}</p>
-            <p>{{ dataApartment.price }}$ /Notte</p>
-
-        </div>
-    </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          :data-bs-target="'#carouselIndicators_' + uniqueId"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          :data-bs-target="'#carouselIndicators_' + uniqueId"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+      <!-- /carosello -->
+      <div class="info-card">
+        <h5 class="card-title pt-2">{{ dataApartment.address }}, {{ dataApartment.city }}</h5>
+        <p>{{ dataApartment.name }}</p>
+        <p>{{ dataApartment.price }}$ /Notte</p>
+      </div>
+    </router-link>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "CardImg",
-    props: {
-        dataApartment: {
-            type: Object,
-        },
+  name: "CardImg",
+  props: {
+    dataApartment: {
+      type: Object,
     },
-    data() {
-        return {
-            activeIndex: 0,
-        };
+  },
+  data() {
+    return {
+      activeIndex: 0,
+    };
+  },
+  computed: {
+    uniqueId() {
+      return "cardImg_" + Math.random().toString(36).substr(2, 9);
     },
-    computed: {
-        uniqueId() {
-            return "cardImg_" + Math.random().toString(36).substr(2, 9);
-        },
-        getAllImages() {
-            if (this.dataApartment) {
-                const images = [this.dataApartment.thumb];
-                this.dataApartment.images.forEach((image) => {
-                    images.push(image.path)
-                })
+    getAllImages() {
+      if (this.dataApartment) {
+        const images = [this.dataApartment.thumb];
+        this.dataApartment.images.forEach((image) => {
+          images.push(image.path);
+        });
 
-                return images;
-
-            }
-        }
-    }
+        return images;
+      }
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .card {
-    background-color: transparent;
-    border: none;
+  background-color: transparent;
+  border: none;
 
-    .carousel-inner {
-        border-radius: 10px;
+  .carousel-inner {
+    border-radius: 10px;
 
-        .carousel-item {
-            img {
-                height: 200px; // Altezza fissa desiderata
-                max-width: 100%; // Imposta l'ampiezza massima come 100% del contenitore
-                object-fit: cover; // Ridimensiona l'immagine in modo da coprire l'area specificata senza distorcere l'aspetto
-            }
-        }
+    .carousel-item {
+      img {
+        height: 200px; // Altezza fissa desiderata
+        max-width: 100%; // Imposta l'ampiezza massima come 100% del contenitore
+        object-fit: cover; // Ridimensiona l'immagine in modo da coprire l'area specificata senza distorcere l'aspetto
+      }
     }
+  }
 
-    .info-card {
-        p {
-            margin: 0.3125rem 0;
-        }
+  .info-card {
+    p {
+      margin: 0.3125rem 0;
     }
+  }
 }
 </style>
--- src/pages/AdvancedSearch.vue
+++ src/pages/AdvancedSearch.vue
@@ -1,154 +1,154 @@
 <template>
-    <div class="container d-flex justify-content-center align-items-center">
-        <div class="custom-container py-5">
-            <!-- search bar -->
-            <div class="input-group rounded pb-5">
-                <input type="search" class="form-control rounded" aria-label="Search" placeholder="Old-ricerca"
-                    aria-describedby="search-addon" />
-                <a href="" class="icon ps-4">
-                    <font-awesome-icon icon="magnifying-glass" />
-                </a>
-                <button type="button" class="btn ps-4 icon" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                    <font-awesome-icon icon="sliders" />
-                </button>
-            </div>
-            <!-- /search bar -->
+  <div class="container d-flex justify-content-center align-items-center">
+    <div class="custom-container py-5">
+      <!-- search bar -->
+      <div class="input-group rounded pb-5">
+        <input
+          type="search"
+          class="form-control rounded"
+          aria-label="Search"
+          placeholder="Old-ricerca"
+          aria-describedby="search-addon"
+        />
+        <a href="" class="icon ps-4">
+          <font-awesome-icon icon="magnifying-glass" />
+        </a>
+        <button type="button" class="btn ps-4 icon" data-bs-toggle="modal" data-bs-target="#exampleModal">
+          <font-awesome-icon icon="sliders" />
+        </button>
+      </div>
+      <!-- /search bar -->
 
-            <!-- Modal -->
-            <ModalFilter />
-            <!-- /Modal -->
+      <!-- Modal -->
+      <ModalFilter />
+      <!-- /Modal -->
 
-            <div class="container">
-                <div class=" row justify-content-center">
-                    <CardImg v-for="apartment in apartments" :dataApartment="apartment" />
-                </div>
-            </div>
+      <div class="container">
+        <div class="row justify-content-center">
+          <CardImg v-for="apartment in apartments" :dataApartment="apartment" />
         </div>
+      </div>
     </div>
+  </div>
 </template>
-  
+
 <script>
-import CardImg from '../components/PageAdvancedSearch/CardImg.vue';
-import ModalFilter from '../components/PageAdvancedSearch/ModalFilter.vue';
-import Apartment from '../api/Apartment';
-import { storeFilter } from '../store/storeFilter';
+import CardImg from "../components/PageAdvancedSearch/CardImg.vue";
+import ModalFilter from "../components/PageAdvancedSearch/ModalFilter.vue";
+import Apartment from "../api/Apartment";
+import { storeFilter } from "../store/storeFilter";
 
 export default {
-    name: "AdvancedSearch",
-    components: {
-        CardImg,
-        ModalFilter
+  name: "AdvancedSearch",
+  components: {
+    CardImg,
+    ModalFilter,
+  },
+  data() {
+    return {
+      storeFilter,
+      apartments: [],
+      searchRadius: 20,
+    };
+  },
+  mounted() {
+    this.searchApartments();
+  },
+  watch: {
+    getRadius(newValue) {
+      this.searchAdvanced();
     },
-    data() {
-        return {
-            storeFilter,
-            apartments: [],
-            searchRadius: 20
-        };
+
+    getMinPrice(newValue) {
+      this.searchAdvanced();
     },
-    mounted() {
-        this.searchApartments();
+
+    getMaxPrice(newValue) {
+      this.searchAdvanced();
     },
-    watch: {
 
-        getRadius(newValue) {
-            this.searchAdvanced();
-        },
-
-        getMinPrice(newValue){
-            this.searchAdvanced();
-        },
-
-        getMaxPrice(newValue){
-            this.searchAdvanced();
-        },
-
-        getRooms(newValue){
-            this.searchAdvanced();
-        },
-
-        getBeds(newValue){
-            this.searchAdvanced();
-        },
-
-        getBaths(newValue){
-            this.searchAdvanced();
-        },
-
-        getServices: {
-        handler(newValue) {
-            this.searchAdvanced();
-        },
-        deep: true
-    }
-
-
+    getRooms(newValue) {
+      this.searchAdvanced();
     },
-    methods: {
-        async searchApartments() {
-            const response = await Apartment.searchByPosition({
-                latitude: this.$route.query.latitude,
-                longitude: this.$route.query.longitude,
-                radius: this.$route.query.radius
-            });
 
-            this.apartments = response;
-        },
-        async searchAdvanced() {
-            const response = await Apartment.searchByPosition({
-                latitude: this.$route.query.latitude,
-                longitude: this.$route.query.longitude,
-                radius: this.storeFilter.range,
-                minPrice: this.storeFilter.minPrice,
-                maxPrice: this.storeFilter.maxPrice,
-                rooms: this.storeFilter.rooms,
-                baths: this.storeFilter.baths,
-                beds: this.storeFilter.beds,
-                services: this.storeFilter.selectedServices
-            })
-            this.apartments = response
-            console.log('call')
-        }
+    getBeds(newValue) {
+      this.searchAdvanced();
     },
-    computed: {
-        getRadius() {
-            return this.storeFilter.range
-        },
 
-        getMinPrice(){
-            return this.storeFilter.minPrice;
-        },
+    getBaths(newValue) {
+      this.searchAdvanced();
+    },
 
-        getMaxPrice(){
-            return this.storeFilter.maxPrice;
-        },
+    getServices: {
+      handler(newValue) {
+        this.searchAdvanced();
+      },
+      deep: true,
+    },
+  },
+  methods: {
+    async searchApartments() {
+      const response = await Apartment.searchByPosition({
+        latitude: this.$route.query.latitude,
+        longitude: this.$route.query.longitude,
+        radius: this.$route.query.radius,
+      });
 
-        getRooms(){
-            return this.storeFilter.rooms;
-        },
+      this.apartments = response;
+    },
+    async searchAdvanced() {
+      const response = await Apartment.searchByPosition({
+        latitude: this.$route.query.latitude,
+        longitude: this.$route.query.longitude,
+        radius: this.storeFilter.range,
+        minPrice: this.storeFilter.minPrice,
+        maxPrice: this.storeFilter.maxPrice,
+        rooms: this.storeFilter.rooms,
+        baths: this.storeFilter.baths,
+        beds: this.storeFilter.beds,
+        services: this.storeFilter.selectedServices,
+      });
+      this.apartments = response;
+      console.log("call");
+    },
+  },
+  computed: {
+    getRadius() {
+      return this.storeFilter.range;
+    },
 
-        getBaths(){
-            return this.storeFilter.baths;
-        },
+    getMinPrice() {
+      return this.storeFilter.minPrice;
+    },
 
-        getBeds(){
-            return this.storeFilter.beds;
-        },
+    getMaxPrice() {
+      return this.storeFilter.maxPrice;
+    },
 
-        getServices(){
-            return this.storeFilter.selectedServices;
-        }
-    }
+    getRooms() {
+      return this.storeFilter.rooms;
+    },
+
+    getBaths() {
+      return this.storeFilter.baths;
+    },
+
+    getBeds() {
+      return this.storeFilter.beds;
+    },
+
+    getServices() {
+      return this.storeFilter.selectedServices;
+    },
+  },
 };
 </script>
-  
+
 <style lang="scss" scoped>
-@import '../assets/partials/variables';
-
-
+@import "../assets/partials/variables";
 
 .icon {
-    font-size: 1.75rem;
-    color: $custom-black;
+  font-size: 1.75rem;
+  color: $custom-black;
 }
 </style>
