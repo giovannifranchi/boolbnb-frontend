139a1f2da60319c64a8b1d9083afad1213250c57
giovannifranchi <116166763+giovannifranchi@users.noreply.github.com>
2023-06-28 18:57:27 +0200

    Add: random background image

--- src/components/PageHome/AppJumbo.vue
+++ src/components/PageHome/AppJumbo.vue
@@ -1,173 +1,180 @@
 <template>
-    <div class="jumbotron container d-flex align-items-center justify-content-center mt-5 mb-5">
-        <form class="form-container d-flex flex-column" @submit.prevent="sendPositionButton()">
-
-            <h2>Find where to stay</h2>
-            <label for="search" class="pb-3">Book unique accommodations, homes, and more on with us!</label>
-            <input placeholder="Search here" type="text" name="search" id="search" v-model="inputSearch" @input="autoComplete(inputSearch)">
-            <ul v-if="inputSearch">
-                <li class="list" v-for="element in foundedItems" @click="sendPosition(element.position)">
-                    {{ element.address.streetName || '' + ' '
-                        +
-                        element.address.municipality + ' ' +
-                        element.address.country
-                    }}
-                </li>
-            </ul>
-            <div class="button-container">
-                <button type="submit">Search</button>
-            </div>
-        </form>
-        <div class="image-container" v-if="randomHighlight">
-            <img :src="randomHighlight.thumb" alt="">
+  <div :style="{
+  backgroundImage: 'url(' + cities[randomInt(0, cities.length - 1)] + ')',
+  backgroundPosition: 'center',
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat'
+}">
+    <div class="jumbotron container d-flex align-items-center justify-content-center mb-5">
+      <form class="form-container d-flex flex-column" @submit.prevent="sendPositionButton()">
+        <h2>Find where to stay</h2>
+        <label for="search" class="pb-3">Book unique accommodations, homes, and more on with us!</label>
+        <input
+          placeholder="Search here"
+          type="text"
+          name="search"
+          id="search"
+          v-model="inputSearch"
+          @input="autoComplete(inputSearch)"
+        />
+        <ul v-if="inputSearch">
+          <li class="list" v-for="element in foundedItems" @click="sendPosition(element.position)">
+            {{ element.address.streetName || "" + " " + element.address.municipality + " " + element.address.country }}
+          </li>
+        </ul>
+        <div class="button-container">
+          <button type="submit">Search</button>
         </div>
+      </form>
+      <div class="image-container" v-if="randomHighlight">
+        <img :src="randomHighlight.thumb" alt="" />
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import { useRouter } from 'vue-router';
-import { mapGetters } from 'vuex';
-import Search from '../../api/Search'
+
+import { mapGetters } from "vuex";
+import Search from "../../api/Search";
 
 export default {
-    name: "AppJumbo",
-    data() {
-        return {
-            inputSearch: '',
-            foundedItems: [],
-            defaultRadius: 20
-        }
-    },
-    computed: {
-        ...mapGetters(['getHighlighted']),
+  name: "AppJumbo",
+  data() {
+    return {
+      inputSearch: "",
+      foundedItems: [],
+      defaultRadius: 20,
+      cities: ['src/assets/images/rome.webp', 'src/assets/images/bern.jpg', 'src/assets/images/copenaghen.jpg', 'src/assets/images/dublin.jpg', 'src/assets/images/prague.jpg', 'src/assets/images/lisbon.webp', 'src/assets/images/lisbon.webp', 'src/assets/images/tallinn.jpg' ]
+    };
+  },
+  computed: {
+    ...mapGetters(["getHighlighted"]),
 
-        randomHighlight() {
-            if (this.getHighlighted) {
-                return this.getHighlighted[this.randomInt(0, this.getHighlighted.length - 1)];
-            }
-        }
+    randomHighlight() {
+      if (this.getHighlighted) {
+        return this.getHighlighted[this.randomInt(0, this.getHighlighted.length - 1)];
+      }
     },
-    methods: {
-        randomInt(min, max) {
-            return Math.floor(Math.random() * (max - min) + min);
+  },
+  methods: {
+    randomInt(min, max) {
+      return Math.floor(Math.random() * (max - min) + min);
+    },
+    async autoComplete(inputSearch) {
+      const response = await Search.autocomplete(inputSearch);
+      this.foundedItems = response.results;
+    },
+    sendPosition(object) {
+      this.$router.push({
+        name: "AdvancedSearch",
+        query: {
+          latitude: object.lat,
+          longitude: object.lon,
+          radius: this.defaultRadius,
         },
-        async autoComplete(inputSearch) {
-            const response = await Search.autocomplete(inputSearch)
-            this.foundedItems = response.results
+      });
+    },
+    sendPositionButton() {
+      this.$router.push({
+        name: "AdvancedSearch",
+        query: {
+          latitude: this.foundedItems[0].position.lat,
+          longitude: this.foundedItems[0].position.lon,
+          radius: this.defaultRadius,
         },
-        sendPosition(object) {
-            this.$router.push({
-                name: 'AdvancedSearch',
-                query: {
-                    latitude: object.lat,
-                    longitude: object.lon,
-                    radius: this.defaultRadius
-                }
-            });
-        },
-        sendPositionButton() {
-            this.$router.push({
-                name: 'AdvancedSearch',
-                query: {
-                    latitude: this.foundedItems[0].position.lat,
-                    longitude: this.foundedItems[0].position.lon,
-                    radius: this.defaultRadius
-                }
-            });
-        }
-    }
-}
+      });
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-@import '../../assets/partials/variables';
+@import "../../assets/partials/variables";
+
+
 
 .form-container {
-    position: relative;
-    transform: translateX(5%);
-    padding: 30px;
-    background-color: white;
-    box-shadow: 0px 0px 20px 16px rgba(17, 17, 26, 0.18);
-    border-radius: 10px;
+  position: relative;
+  transform: translateX(5%);
+  padding: 30px;
+  background-color: white;
+  box-shadow: 0px 0px 20px 16px rgba(17, 17, 26, 0.18);
+  border-radius: 10px;
 
-    ul {
-        padding: 0;
+  ul {
+    padding: 0;
 
-        li {
-            margin: 4px 0;
-            cursor: pointer;
-
-            a {
-                color: $custom-black;
-                text-decoration: none;
-
-            }
-        }
-    }
-
-    input,
     li {
-        border: 1px solid $custom-black;
-        font-size: 20px;
-        border-radius: 5px;
-        padding: 5px 20px;
-        width: 400px;
+      margin: 4px 0;
+      cursor: pointer;
+
+      a {
+        color: $custom-black;
+        text-decoration: none;
+      }
     }
+  }
 
-    li:hover {
-        background-color: rgba(0, 0, 0, 0.144);
+  input,
+  li {
+    border: 1px solid $custom-black;
+    font-size: 20px;
+    border-radius: 5px;
+    padding: 5px 20px;
+    width: 400px;
+  }
+
+  li:hover {
+    background-color: rgba(0, 0, 0, 0.144);
+  }
+
+  .button-container {
+    display: flex;
+
+    button {
+      border: none;
+      width: 100%;
+      margin-top: 25px;
+      padding: 10px 0;
+      font-size: 20px;
+      font-weight: bolder;
+      border-radius: 10px;
+      color: $custom-white;
+      background-color: $custom-green;
     }
-
-    .button-container {
-        display: flex;
-
-        button {
-            border: none;
-            width: 100%;
-            margin-top: 25px;
-            padding: 10px 0;
-            font-size: 20px;
-            font-weight: bolder;
-            border-radius: 10px;
-            color: $custom-white;
-            background-color:$custom-green;
-        }
-    }
-
-
+  }
 }
 
 .image-container {
-    width: 1000px;
-    height: 600px;
-    transform: translateX(-5%);
-    z-index: -1;
-    border-radius: 10px;
-    overflow: hidden;
-    box-shadow: 0px 0px 20px 16px rgba(17, 17, 26, 0.18);
-    img {
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-
-    }
+  width: 1000px;
+  height: 600px;
+  transform: translateX(-5%);
+  z-index: -1;
+  border-radius: 10px;
+  overflow: hidden;
+  box-shadow: 0px 0px 20px 16px rgba(17, 17, 26, 0.18);
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
 }
 
 @media (max-width: 992px) {
-    .jumbotron{
-        padding: 0;
-    }
-    .image-container {
-      display:none;
-    }
-    .form-container{
-        transform: translateX(0%);
-        width: 100%;
-        input,
+  .jumbotron {
+    padding: 0;
+  }
+  .image-container {
+    display: none;
+  }
+  .form-container {
+    transform: translateX(0%);
+    width: 100%;
+    input,
     li {
-        width: 100%;
-    }
-        
+      width: 100%;
     }
   }
+}
 </style>
